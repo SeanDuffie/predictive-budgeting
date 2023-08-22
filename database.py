844f0809ac79569428c5754d23380ad186f27e5a
SeanDuffie <sean.duffie@tamu.edu>
2023-08-21 22:58:51 -0500

    Started the database pipeline

---
+++ database.py
@@ -0,0 +1,14 @@
+""" responsible for loading from and saving to csv files
+"""
+from transaction import Transaction#, Expense, Income
+
+class Database:
+    def load_save(self):
+        pass
+
+    def add_entry(self):
+        pass
+
+if __name__ == "__main__":
+    first = Transaction("rent", 633.34, 1, "rent")
+    print(first)
