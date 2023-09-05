558e2034fef6a35c60597eed5710502ef555090d
Sean Duffie <sduffie@lynntech.com>
2023-09-05 15:41:57 -0500

    Feature: Objectify the stock process Feature: Add functions for basic statistics (SMA, Volatility, RSI) DOCS: Update Readme with setup instructions and basic TODO

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # predictive-budgeting
+
 I need a way to manage my money that is more hands-off than manually creating excel sheets and guessing. This is the repository where I will explore my ideas for solutions to this.
+
+# Setup/Installation/Usage
+
+1. Clone from GitHub Repo
+    ``` cmd
+    git clone https://github.com/SeanDuffie/predictive-budgeting.git
+    cd ./predictive-budgeting
+    ```
+2. Install the environment dependencies
+    ``` cmd
+    python -m pip install --upgrade pip
+    pip install -r ./requirements.txt
+    ```
+3. Drop any new data entries into the database folder
+4. Run main.py
+5. Open the Flask Server in a browser at 127.0.0.1.
+TODO: Make this more permanent with a port forward and noip.com?
+
+# TODO
+
+1. Budget
+    - [ ] Automatically read in CSV
+    - [ ] Compare new CSV to existing database, compile non duplicates into a cumulative database.
+    - [ ] Categorize expenses and incomes
+    - [ ] Launch into a Flask Server for graphs and interactive data
+        - [ ] Pie Chart for how much in each category for both expenses and 
+2. Stocks
+    - [ ] Milestones to indicate potential causes of pattern changes
+3. Loans
+    - [ ] Amortization chart?
--- stock.py
+++ stock.py
@@ -1,37 +1,107 @@
 import yfinance as yf
+import pandas as pd
+import numpy as np
 
-msft = yf.Ticker("MSFT")
-print(msft)
-"""
-returns
-<yfinance.Ticker object at 0x1a1715e898>
-"""
+class Stock:
+    def __init__(self, title) -> None:
+        self.title = title
+        self.refresh()
 
-# get stock info
-msft.info
+    def refresh(self) -> None:
+        """ Updates the current stock data from YFinance """
+        self.stock = yf.Ticker(self.title)
+        self.df = self.stock.history
 
-"""
-returns:
-{
- 'quoteType': 'EQUITY',
- 'quoteSourceName': 'Nasdaq Real Time Price',
- 'currency': 'USD',
- 'shortName': 'Microsoft Corporation',
- 'exchangeTimezoneName': 'America/New_York',
-  ...
- 'symbol': 'MSFT'
-}
-"""
+        self.SMA()
 
-# get historical market data, here max is 5 years.
-msft.history(period="max")
-"""
-returns:
-              Open    High    Low    Close      Volume  Dividends  Splits
-Date
-1986-03-13    0.06    0.07    0.06    0.07  1031788800        0.0     0.0
-1986-03-14    0.07    0.07    0.07    0.07   308160000        0.0     0.0
-...
-2019-11-12  146.28  147.57  146.06  147.07    18641600        0.0     0.0
-2019-11-13  146.74  147.46  146.30  147.31    16295622        0.0     0.0
-"""
+    def SMA(self, feature: str, window_size: int) -> pd.DataFrame:
+        """ Simple Moving Average
+            Calculates the average of a certain feature over a given window of time
+
+            Parameters:
+            - feature (str) - what column to measure
+            - window_size (int) - what time period to measure
+
+            Returns:
+            - df - pandas DataFrame with new feature column appended
+        """
+        new_col = f"MA_{feature}_{window_size}"
+        self.df[new_col] = self.df[feature].rolling(window=window_size).mean()
+        return self.df
+    
+    def Volatility(self, feature: str, window_size: int) -> pd.DataFrame:
+        """ Volatility
+            Calculates the Volatility of a certain feature over a given window of time
+
+            Parameters:
+            - feature (str) - what column to measure
+            - window_size (int) - what time period to measure
+
+            Returns:
+            - df - pandas DataFrame with new feature column appended
+        """
+        new_col = f"VOL_{feature}_{window_size}"
+        returns = np.log(self.df[feature]/self.df[feature].shift())
+        returns.fillna(0, inplace=True)
+        self.df[new_col] = returns.rolling(window=window_size).std()*np.sqrt(window_size)
+        return self.df
+    
+    def RSI(self, feature: str, window_size: int) -> pd.DataFrame:
+        """ Relative Strength Index
+            Calculates the Momentum of the stock based on previous data
+
+            Parameters:
+            - feature (str) - what column to measure
+            - window_size (int) - what time period to measure
+
+            Returns:
+            - df - pandas DataFrame with new feature column appended
+        """
+        new_col = f"RSI_{feature}_{window_size}"
+        delta = self.df[feature].diff()
+        delta = delta [1:]
+        up, down = delta.clip(lower=0), delta.clip(upper=0)
+        roll_up = up.rolling(window_size).mean()
+        roll_down = down.abs().rolling(window_size).mean()
+        rs = roll_up / roll_down
+        rsi = 100 - (100 / (1 + rs))
+        self.df[new_col] = rsi
+        return self.df
+
+    def print_info(self):
+        """
+        returns:
+        {
+        'quoteType': 'EQUITY',
+        'quoteSourceName': 'Nasdaq Real Time Price',
+        'currency': 'USD',
+        'shortName': 'Microsoft Corporation',
+        'exchangeTimezoneName': 'America/New_York',
+        ...
+        'symbol': 'MSFT'
+        }
+        """
+
+        # get stock info
+        print(self.stock.info)
+
+    def print_history(self):
+        """
+        returns:
+                    Open    High    Low    Close      Volume  Dividends  Splits
+        Date
+        1986-03-13    0.06    0.07    0.06    0.07  1031788800        0.0     0.0
+        1986-03-14    0.07    0.07    0.07    0.07   308160000        0.0     0.0
+        ...
+        2019-11-12  146.28  147.57  146.06  147.07    18641600        0.0     0.0
+        2019-11-13  146.74  147.46  146.30  147.31    16295622        0.0     0.0
+        """
+        # get historical market data, here max is 5 years.
+        print(self.stock.history(period="max"))
+
+if __name__ == "__main__":
+    s1 = Stock("T")
+    s2 = Stock("MSFT")
+
+    s1.print_history()
+    s2.print_history()
