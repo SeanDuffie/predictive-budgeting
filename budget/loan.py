c3dbff3ad7b92e2db863c63c0a003ed04207b7db
SeanDuffie <duffiesean@gmail.com>
2024-02-27 17:42:30 -0500

    Added amortization table to loan.py

--- budget/loan.py
+++ budget/loan.py
@@ -2,26 +2,41 @@
     References:
     - https://www.bankrate.com/mortgages/amortization-calculator/
 """
+import pandas as pd
 
 class Loan():
     def __init__(self, amount: float, apr: float, term: int, name: str = "Loan"):
+        self.name = name
         self.loan_amount = amount
         self.principal = amount
         self.apr = apr
         self.mpr = apr / 12
 
+        self.term = term
+        self.min_payment = (self.loan_amount * self.mpr) / (1 - (1 + self.mpr) ** -self.term)
+        print(f"{self.min_payment=}")
+
         self.interest_table = []
         self.principal_table = []
         self.total_interest = 0
 
-        payments = [[principal, 0, 0, 0]]
-        total_interest = 0
 
-        while self.principal > 0 and term_remaining > 0:
-            payment = make_payment(monthly_payment)
-            principal = payment[0]
-            term_remaining -= 1
+    def amor_table(self, monthly_payment = None):
+        if monthly_payment is None:
+            monthly_payment = self.min_payment
+
+        payments = [[self.principal, 0, 0, 0]]
+        total_interest = 0
+        prin = self.principal
+        term_left = self.term
+
+        while prin > 0 and term_left > 0:
+            payment = self.make_payment(prin, monthly_payment)
+
+            prin = payment[0]
+            term_left -= 1
             total_interest += payment[2]
+
             payment.append(total_interest)
             payments.append(payment)
 
@@ -30,13 +45,26 @@ class Loan():
                                                 'Current Principal Payment',
                                                 'Current Interest Payment',
                                                 'Total Interest Paid'])
-        return amortization_table
+        print(amortization_table)
 
-    def make_payment(self, monthly_payment):
-        interest = self.principal*self.apr/12
+    def make_payment(self, principal, monthly_payment):
+        # Calculate the amount of interest generated this month, then the amount applied to they 
+        current_interest_payment = principal * self.mpr
+        current_principal_payment = monthly_payment - current_interest_payment
 
-    def minimum_payment(self, timeframe):
-        pass
+        principal -= current_principal_payment
+
+        return [round(principal, 2), round(current_principal_payment, 2), round(current_interest_payment, 2)]
+
 
 if __name__ == "__main__":
-    Loan(10000, .033, 120)
+    print("Student Loans:")
+    mohela = Loan(10000, .033, 120)
+    mohela.amor_table()
+    mohela.amor_table(500)
+    mohela.amor_table(1000)
+
+    mort = Loan(300000, .06, 180)
+    mort.amor_table()
+    mort.amor_table(2500)
+    mort.amor_table(3500)
