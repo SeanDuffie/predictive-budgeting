ca5bd629fc428a93020853e3a30d804341302942
SeanDuffie <duffiesean@gmail.com>
2024-03-05 14:39:07 -0500

    Started process on Budget Class

--- budget/budget.py
+++ budget/budget.py
@@ -1,4 +1,24 @@
 """_summary_
 """
 
-input("Annual Income:")
+
+class Budget():
+    def __init__(self, gross: float):
+        # Collect Income and Region
+        self.gross_income = gross
+        self.taxes = self.calculate_taxes()
+
+        # Adjust income for taxes
+        self.income = self.gross_income - self.taxes
+
+    def calculate_taxes(self):
+        return 0
+
+    def __str__(self):
+        bgt_str = f"Budget: gross=${self.gross_income}\n"
+        bgt_str += f"\t taxes = -$({self.taxes})"
+        return bgt_str
+
+if __name__ == "__main__":
+    bgt = Budget(104000)
+    print(bgt)
