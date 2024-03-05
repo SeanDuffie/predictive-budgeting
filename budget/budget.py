b6de377a07bd190cdca91d52a842655501c48cc7
SeanDuffie <duffiesean@gmail.com>
2024-03-05 17:11:36 -0500

    Started to add info on taxes

--- budget/budget.py
+++ budget/budget.py
@@ -12,6 +12,28 @@ class Budget():
         self.income = self.gross_income - self.taxes
 
     def calculate_taxes(self):
+        """_summary_
+
+        Resources:
+        - https://smartasset.com/taxes/income-taxes
+        - Federal: 
+        - FICA: https://smartasset.com/taxes/all-about-the-fica-tax
+            FICA (Federal Insurance Contributions Act) is a tax that funds Social Security and
+            Medicare. This tax about 15.3% of the worker's gross income, and is split between the
+            Employer and Employee. (About 7.65% for just the Employee). Self-Employed workers are
+            stuck with the full 15.3%, however they can deduct the extra half when filing taxes.
+            12.4% goes towards Social Security, 2.9% goes towards Medicare.
+
+            There is a cap on the amount of income taxed for Social Security (~$168,600 for 2024),
+            however there is no cap on Medicare, and there is an additional Medicare Tax that
+            high-income individuals must pay (+0.9%).
+        - State: 
+        - Local: 
+
+        Returns:
+            _type_: _description_
+            
+        """
         return 0
 
     def __str__(self):
