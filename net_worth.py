712647d52325402839f2359599b51a2bafa12b82
SeanDuffie <duffiesean@gmail.com>
2024-03-11 15:29:52 -0400

    Integrated Asset into net_worth.py Added documentation to different functions

--- net_worth.py
+++ net_worth.py
@@ -3,9 +3,10 @@
 import datetime
 
 import pandas as pd
-from net_worth import Database, Loan, Savings
+from net_worth import Database, Loan, Savings, Asset
 
 
+# FIXME: Rename to "Portfolio" soon
 class NetWorth():
     """ This class is intended to help measure the overall net worth of my assets
     """
@@ -14,14 +15,29 @@ class NetWorth():
         self.db = Database(db_name=db_name)
 
         # Preview Existing data
-        self.loans = []
-        self.assets = []
+        self.loans: list[Loan] = []
+        self.assets: list[Asset] = []
         self.investments = []
-        self.savings = []
+        self.savings: list[Savings] = []
 
-    def add_loan(self, amount, apr, start, end, name):
+    def add_loan(self, amount: float, apr: float, start: datetime.date, end: datetime.date, name: str):
+        """ Add a debt to your portfolio
+
+        Args:
+            amount (float): amount owed initially
+            apr (float): interest rate on loan
+            start (datetime.date): day that the loan started accruing interest
+            end (datetime.date): date the loan is due to be paid off
+            name (str): User friendly name for the loan
+        """
         # Prompt for additional loans
-        new_loan = Loan(amount=amount, apr=apr, start=start, term=end, name=name)
+        new_loan = Loan(
+            amount=amount,
+            apr=apr,
+            start=start,
+            term=end,
+            name=name
+        )
         self.loans.append(new_loan)
 
     def remove_loan(self):
@@ -45,9 +61,23 @@ class NetWorth():
 
         # Update database table
 
-    def add_asset(self):
+    def add_asset(self, init_value: float, start: datetime.date, apr: float, name: str):
+        """ Add an Asset (such as a car or house) to your portfolio
+
+        Args:
+            init_value (float): cost paid for the asset
+            start (datetime.date): date that the asset was purchased (used for projection)
+            apr (float): expected appreciation (house) or depreciation (car) of the object
+            name (str): User friendly name for the asset
+        """
         # Prompt for additional assets
-        pass
+        new_asset = Asset(
+            init_value=init_value,
+            expected_apr=apr,
+            start=start,
+            name=name
+        )
+        self.assets.append(new_asset)
 
     def remove_asset(self):
         # Identify which asset to remove
@@ -64,6 +94,14 @@ class NetWorth():
         pass
 
     def add_savings(self, deposit: float, start: datetime.date, apr: float, recur: float = 0):
+        """ Add a savings account to be tracked in your portfolio
+
+        Args:
+            deposit (float): Initial ammount in account when tracking started
+            start (datetime.date): date that tracking began
+            apr (float): interest rate on account (mainly for HYSA)
+            recur (float, optional): Recurring deposits going to the account. Defaults to 0.
+        """
         # Prompt for additional Savings
         new_sav = Savings(deposit=deposit, start=start, apr=apr, recur=recur)
         self.savings.append(new_sav)
@@ -83,6 +121,13 @@ class NetWorth():
         del self.savings[index]
 
     def calculate_gross(self):
+        """ Sums together the values of all portfolio
+
+        TODO: integrate timeline, take date as parameter
+
+        Returns:
+            float: Sum of all components of portfolio
+        """
         # Add value of all Savings
         savings = 0
         for sav in self.savings:
@@ -90,44 +135,79 @@ class NetWorth():
 
         # Add value of all Investments
         invest = 0
+        for inv in self.investments:
+            savings += inv.balance
 
         # Add value of all Assets
         assets = 0
+        for ast in self.assets:
+            assets += ast.value
 
         gross = savings + invest + assets
 
         return gross
 
     def calculate_debts(self):
+        """ Sum together the current value of all debts
+
+        TODO: integrate timeline, take date as parameter
+
+        Returns:
+            float: Sum of all loans/debts (TODO: at a given time)
+        """
         # Subtract value of all Loans
         loans = 0
         for loan in self.loans:
-            # loans += loan.get_balance(datetime.datetime.today())
-            loans += loan.get_balance(datetime.datetime(2024, 4, 5))
+            # loans += loan.get_balance(datetime.date.today())
+            loans += loan.get_balance(datetime.date(2024, 4, 5))
 
         return loans
 
     def calculate_net(self):
+        """ Calculate the net worth
+
+        Returns:
+            float: Net Worth
+        """
+        print(f"{len(self.savings)} Savings | {len(self.investments)} Investments | {len(self.assets)} Assets | {len(self.loans)} Loans")
         gross = self.calculate_gross()
         debts = self.calculate_debts()
 
         total = gross - debts
 
-        print(f"Total net worth: ${total}")
+        print(f"Total net worth: ${total} (${gross} - ${debts})")
 
         return total
 
     def calculate_ratio(self):
+        """ Calculate the ratio of assets compared to debts
+        
+        FIXME: I think I did this wrong, should it be more of a budget thing than a portfolio?
+
+        Returns:
+            float: Ratio of debts to assets. 0 is even, negative is in debt, positive is best
+        """
         gross = self.calculate_gross()
         debts = self.calculate_debts()
 
         ratio = (gross - debts) / (gross + debts)
 
-        print(f"Debt to Income Ratio: {ratio}")
+        # print(f"Debt to Income Ratio: {ratio}")
 
         return ratio
 
-    def project_net(self, ) -> pd.DataFrame:
+    def project_net(self, day: datetime.date) -> pd.DataFrame:
+        """ Projects net worth over a period of time
+
+        FIXME: I think this needs to be an individual function, not a dataframe
+        TODO: Dataframes should be stored in each individual object, this function should just parse them
+
+        Args:
+            day (datetime.date): date of desired portfolio calculation
+
+        Returns:
+            pd.DataFrame: projection of portfolio over time TODO: replace this with a single float or a tuple
+        """
         # TODO: Loop throught each "update" function until reaching the current date
         # TODO: Each Update function should calculate interest and recurring changes
         # TODO: Call the current value function for each item
@@ -145,13 +225,20 @@ if __name__ == "__main__":
 
     my_networth.add_savings(
         deposit=10000,
-        start=datetime.date(2024, 4, 5),
+        start=datetime.date(2024, 3, 5),
         apr=0.0435,
         recur=2000
     )
 
+    my_networth.add_savings(
+        deposit=10000,
+        start=datetime.date(2024, 3, 5),
+        apr=0.07,
+        recur=500
+    )
+
     my_networth.add_loan(
-        start=datetime.datetime(2024, 4, 5),
+        start=datetime.datetime(2024, 3, 5),
         end=datetime.datetime(2033, 9, 5),
         amount=5677.25,
         apr=.025,
@@ -159,11 +246,40 @@ if __name__ == "__main__":
     )
 
     my_networth.add_loan(
-        start=datetime.datetime(2024, 4, 5),
+        start=datetime.datetime(2024, 3, 5),
         end=datetime.datetime(2033, 9, 5),
         amount=4777.83,
         apr=.0348,
         name="Student Loan #2"
+    )
+
+    my_networth.calculate_net()
+    # my_networth.calculate_ratio()
+
+    # Home cost is an Asset
+    home_val = 330000
+    purchase_date = datetime.date(2024, 3, 1)
+    my_networth.add_asset(
+        init_value=home_val,
+        start=purchase_date,
+        apr=0.08,
+        name="House"
+    )
+
+    # Down payment is subtracted from savings
+    down_payment = home_val * 0.2
+    my_networth.savings[0].modify_balance(-down_payment, purchase_date, "Placed down payment on house")
+
+    # Mortgage is home cost minus down payment
+    mort = home_val - down_payment
+    mort_date = purchase_date + datetime.timedelta(3650)
+    mort_term = 120
+    my_networth.add_loan(
+        start=purchase_date,
+        end=mort_term,
+        amount=mort,
+        apr=.06,
+        name="Mortgage"
     )
 
     my_networth.calculate_net()
