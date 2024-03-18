8bcdcea1208c79425b21ec2d8a3914c294efcf2c
SeanDuffie <sean.duffie@tamu.edu>
2024-03-17 21:22:41 -0400

    Added runner for income

--- budget/income.py
+++ budget/income.py
@@ -1,10 +1,12 @@
 class Income():
-    def __init__(self, annual: float):
+    def __init__(self, annual: float, state: str):
         self.gross_annual = annual
         self.gross_monthly = annual / 12
-        
+
         self.calculate_tax()
-        
+
     def calculate_tax(self):
         pass
-    
+
+if __name__ == "__main__":
+    Income(100000, "GA")
