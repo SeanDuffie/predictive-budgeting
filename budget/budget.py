516cc9a091e3744c6975a471be5beeb85ce73c7b
SeanDuffie <duffiesean@gmail.com>
2024-03-20 14:12:31 -0400

    Moved calculate tax to income class Started to lay out template for applying and generating budgets Added templates for handling keyword arguments for special cases Income class now handles gross, basic tax, and net for annual, monthly, and weekly intervals

--- budget/budget.py
+++ budget/budget.py
@@ -1,45 +1,57 @@
 """_summary_
 """
+from income import Income
+
+distribution = {
+    "Housing": (None, 0.2),
+    "Utilities": (None, 0),
+    "Internet": (90, None),
+    "Transportation": 0,
+    "Food": 0.03,
+    "Insurance": 0,
+    "Debts (minimum)": (100, 0),
+    "Emergency Savings": 0,
+    "Debts (extra)": 0,
+    "HYSA": 0,
+    "Investments": 0,
+    
+}
 
 
 class Budget():
-    def __init__(self, gross: float):
+    """_summary_
+    """
+    def __init__(self, gross: float, state: str, **flags):
+        # TODO: add flags for things like paycheck/bill interval
+        for key, value in flags.items():
+            print("{}: {}".format(key,value))
+
         # Collect Income and Region
-        self.gross_income = gross
-        self.taxes = self.calculate_taxes()
+        self.income = Income(gross, state)
+        self.taxes = self.income.taxes / 12
+        self.net = self.income.net_monthly
+        self.remaining = self.net
 
-        # Adjust income for taxes
-        self.income = self.gross_income - self.taxes
+    def generate_budget(self):
+        return distribution
 
-    def calculate_taxes(self):
-        """_summary_
+    def apply_budget(self, budget_dict):
 
-        Resources:
-        - https://smartasset.com/taxes/income-taxes
-        - Federal: 
-        - FICA: https://smartasset.com/taxes/all-about-the-fica-tax
-            FICA (Federal Insurance Contributions Act) is a tax that funds Social Security and
-            Medicare. This tax about 15.3% of the worker's gross income, and is split between the
-            Employer and Employee. (About 7.65% for just the Employee). Self-Employed workers are
-            stuck with the full 15.3%, however they can deduct the extra half when filing taxes.
-            12.4% goes towards Social Security, 2.9% goes towards Medicare.
-
-            There is a cap on the amount of income taxed for Social Security (~$168,600 for 2024),
-            however there is no cap on Medicare, and there is an additional Medicare Tax that
-            high-income individuals must pay (+0.9%).
-        - State: 
-        - Local: 
-
-        Returns:
-            float: amount of taxes to deduct from gross
-            
-        """
-        return 0
-
-    def __str__(self):
-        bgt_str = f"Budget: gross=${self.gross_income}\n"
-        bgt_str += f"\t taxes = -$({self.taxes})"
-        return bgt_str
+        # TODO: (FOO1) Subtract Deductibles
+        
+        # TODO: (FOO2) Subtract Employer 401K Match
+        
+        # TODO: (FOO3) Subtract High-Interest Debt
+        
+        # TODO: Subtract Expenses
+            # Rent
+            # Utilities
+            # Groceries
+            # 
+        
+        # TODO: (FOO4) If Emergency Reserves is not full, contribute
+        
+        
 
 if __name__ == "__main__":
     bgt = Budget(104000)
--- budget/income.py
+++ budget/income.py
@@ -1,12 +1,67 @@
+"""_summary_
+"""
+
 class Income():
-    def __init__(self, annual: float, state: str):
+    """_summary_
+    
+    TODO: Add arguments for self-employed, or other special cases
+    """
+    def __init__(self, annual: float, state: str, **flags):
+        # TODO: Implement flags
+        for key, value in flags.items():
+            print("{}: {}".format(key,value))
+
         self.gross_annual = annual
         self.gross_monthly = annual / 12
+        self.gross_weekly = self.gross_annual / 52
 
-        self.calculate_tax()
+        self.taxes = self.calculate_tax(state=state)
 
-    def calculate_tax(self):
-        pass
+        self.net_annual = self.gross_annual - self.taxes
+        self.net_monthly = self.net_annual / 12
+        self.net_weekly = self.net_annual / 52
+
+    def calculate_tax(self, state: str):
+        """_summary_
+
+        Resources:
+        - https://smartasset.com/taxes/income-taxes
+        - Federal: 
+        - FICA: https://smartasset.com/taxes/all-about-the-fica-tax
+            FICA (Federal Insurance Contributions Act) is a tax that funds Social Security and
+            Medicare. This tax about 15.3% of the worker's gross income, and is split between the
+            Employer and Employee. (About 7.65% for just the Employee). Self-Employed workers are
+            stuck with the full 15.3%, however they can deduct the extra half when filing taxes.
+            12.4% goes towards Social Security, 2.9% goes towards Medicare.
+
+            There is a cap on the amount of income taxed for Social Security (~$168,600 for 2024),
+            however there is no cap on Medicare, and there is an additional Medicare Tax that
+            high-income individuals must pay (+0.9%).
+        - State: 
+        - Local: 
+        
+        Args:
+            state (str): state of residence for paying taxes
+
+        Returns:
+            float: amount of taxes to deduct from gross
+            
+        """
+        state_tax = self.gross_annual * 0.0525
+        federal_tax = self.gross_annual * 0.24
+
+        tax_burden = state_tax + federal_tax
+
+        return tax_burden
+
+    def __str__(self) -> str:
+        bgt_str = f"Gross = ${self.gross_annual}\n"
+        bgt_str += f"\t-taxes = -$({self.taxes})\n"
+        bgt_str += f"Net = $({self.net_annual})\n"
+        bgt_str += f"\t Per Month = $({self.net_monthly})\n"
+        bgt_str += f"\t Per Week = $({self.net_weekly})\n"
+
+        return bgt_str
 
 if __name__ == "__main__":
-    Income(100000, "GA")
+    print(Income(104000, "GA"))
