fdda16bbe4a8598901e9b81965f53d794355bf87
SeanDuffie <duffiesean@gmail.com>
2024-03-20 18:45:03 -0400

    Added an add_deductible function Added many notes on taxes and AGI

--- budget/income.py
+++ budget/income.py
@@ -15,16 +15,64 @@ class Income():
         self.gross_monthly = annual / 12
         self.gross_weekly = self.gross_annual / 52
 
-        self.taxes = self.calculate_tax(state=state)
+        self.deductibles = []
+        self.agi = self.gross_annual
+        self.state = state
+
+        self.taxes = self.calculate_tax()
 
         self.net_annual = self.gross_annual - self.taxes
         self.net_monthly = self.net_annual / 12
         self.net_weekly = self.net_annual / 52
 
-    def calculate_tax(self, state: str):
-        """_summary_
+    def add_deductible(self, deduct: float):
+        """ Track deductibles here to properly estimate your AGI for tax responsibility
+
+        TODO: Itemize the deductibles more, rather than just a raw float
+
+        Terms:
+            - Gross Income - amount of income before any taxes
+            - AGI - Adjusted Gross Income, gross income minus some deductions
+            - MAGI - Modified Adjusted Gross Income, TODO
+
+        NOTE: What is Taxable Income?
+            1. Retirement contributions
+                a. Individual Retirement Account (IRA)
+                b. Self-employed retirement contributions
+                c. 
+            2. Charitable Donations (up to 60% of AGI)
+            3. Education Expenses
+            4. Student Loan Interest
+            5. Mortgage Interest ($750,000 or less)
+            6. Medical expenses (over 7.5% AGI)
+            7. State and local Income Tax
+            8. *Personal* Property Tax or Sales taxes (up to IRS threshold)
+            9. Gambling/Investment losses
+        NOTE: You should itemize deductions if the expenses are greater than the Standard Deduction
 
         Resources:
+            - https://www.investopedia.com/ask/answers/051115/what-difference-between-magi-modified-adjusted-gross-income-and-adjusted-gross-income.asp
+            - https://www.investopedia.com/terms/i/itemizeddeduction.asp
+            - https://www.hrblock.com/tax-center/income/how-to-calculate-taxable-income/
+
+        Args:
+            deduct (float): Amount being deducted from Gross Income
+        """
+        self.agi = self.gross_annual
+        self.deductibles.append(deduct)
+
+        for expense in self.deductibles:
+            self.agi -= expense
+        print(f"Adjusted Gross Income (AGI): ${self.agi}")
+
+        self.taxes = self.calculate_tax()
+
+    def calculate_tax(self):
+        """ Use this function to calculate tax liability, you should already know AGI prior to call
+
+        Resources:
+        - https://www.hrblock.com/tax-center/income/how-to-calculate-taxable-income/
+        - https://www.irs.gov/individuals/tax-withholding-estimator
         - https://smartasset.com/taxes/income-taxes
         - Federal: 
         - FICA: https://smartasset.com/taxes/all-about-the-fica-tax
@@ -39,7 +87,7 @@ class Income():
             high-income individuals must pay (+0.9%).
         - State: 
         - Local: 
-        
+
         Args:
             state (str): state of residence for paying taxes
 
