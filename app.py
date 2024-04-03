528c5d18bda7784c365372ad52a3339e8a6681cc
SeanDuffie <duffiesean@gmail.com>
2024-04-03 18:18:08 -0400

    Added template example from digital ocean

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 """ @file app.py
     @author Sean Duffie
     @brief Flask Framework to host the web user interface with the budgeting program
-    
+
     This website allows a more convenient way for the user to utilize the budgeting tools.
     Additionally, this will display the Bokeh data visualization tools and potentially allow users
     to store and load data.
+
+    Resources:
+    - https://www.digitalocean.com/community/tutorials/how-to-use-web-forms-in-a-flask-application
 """
 import os
 
@@ -17,9 +20,15 @@ app = Flask(
     template_folder=f"{RTDIR}/web/templates"
 )
 
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+
 @app.route("/")
-def welcome():
-    return render_template("form.html")
+def index():
+    return render_template("form.html", messages=messages)
 
 if __name__ == "__main__":
     print(RTDIR)
