17aaf694f1b64e9fa3c43548023b1a156a1890d2
SeanDuffie <sean.duffie@tamu.edu>
2024-04-04 01:16:31 -0400

    Added some example bokeh plots

--- main.py
+++ main.py
@@ -68,12 +68,68 @@ if __name__ == "__main__":
     portfolio.calculate_net(TL3)
     portfolio.calculate_ratio(TL3)
 
-    print("\nHYSA history:")
-    for event in portfolio.savings["American Express HYSA"].history:
-        print(f"\t{event}")
+    # print("\nHYSA history:")
+    # for event in portfolio.savings["American Express HYSA"].history.iterrows():
+    #     print(f"\t{event}")
 
-    print("\nETRADE history:")
-    for event in portfolio.savings["Stocks"].history:
-        print(f"\t{event}")
+    # print("\nETRADE history:")
+    # for event in portfolio.savings["Stocks"].history.iterrows():
+    #     print(f"\t{event}")
 
     # Ask if user wants to save to a database
+    import os
+
+    import bokeh.embed
+    import bokeh.plotting
+    import pandas as pd
+
+    RTDIR = os.path.dirname(__file__)
+
+    # create a pandas dataframe
+    # df = pd.DataFrame({'date': pd.to_datetime(['2023-01-01', '2023-01-02', '2023-01-03']), 'value': [1, 2, 3]})
+    df = portfolio.loans["Student Loan #1"].plan
+    df["Date"] = pd.to_datetime(df["Date"])
+
+    # create a Bokeh figure
+    p = bokeh.plotting.figure(title="Loan Change", x_axis_label='Date', y_axis_label='Amount')
+
+    # add a line renderer to the figure
+    p.line(x=df['Date'], y=df['Balance Remaining'], line_width=2)
+    p.line(x=df['Date'], y=df['Applied to Principal'], line_width=2)
+    p.line(x=df['Date'], y=df['Applied to Interest'], line_width=2)
+    p.line(x=df['Date'], y=df['Total Interest'], line_width=2)
+
+    # bokeh.plotting.output_file()
+    bokeh.plotting.save(p, filename=f"{RTDIR}/Loans.html")
+
+    html = bokeh.embed.file_html(p)
+    # print(f"{html=}\n\n\n")
+
+    # json = bokeh.embed.json_item(p)
+    # print(f"{json=}")
+
+    # show the figure
+    # bokeh.plotting.show(p)
+
+    df = portfolio.savings["American Express HYSA"].history
+    df["Date"] = pd.to_datetime(df["Date"])
+
+    # create a Bokeh figure
+    p = bokeh.plotting.figure(title="Savings Change", x_axis_label='Date', y_axis_label='Amount')
+
+    # add a line renderer to the figure
+    p.line(x=df['Date'], y=df['Balance'], line_width=2)
+    p.line(x=df['Date'], y=df['Change'], line_width=2)
+
+    # bokeh.plotting.output_file()
+    bokeh.plotting.save(p, filename=f"{RTDIR}/Savings.html")
+
+    html = bokeh.embed.file_html(p)
+    # print(f"{html=}\n\n\n")
+
+    # json = bokeh.embed.json_item(p)
+    # print(f"{json=}")
+
+    # show the figure
+    # bokeh.plotting.show(p)
+
--- net_worth/viz.py
+++ net_worth/viz.py
@@ -1,14 +1,29 @@
+import os
+
+import bokeh.embed
+import bokeh.plotting
 import pandas as pd
-from bokeh.plotting import figure, show
+import pandas_bokeh
+
+RTDIR = os.path.dirname(__file__)
 
 # create a pandas dataframe
 df = pd.DataFrame({'date': pd.to_datetime(['2023-01-01', '2023-01-02', '2023-01-03']), 'value': [1, 2, 3]})
 
 # create a Bokeh figure
-p = figure(title="Line Plot", x_axis_label='Date', y_axis_label='Value')
+p = bokeh.plotting.figure(title="Line Plot", x_axis_label='Date', y_axis_label='Value')
 
 # add a line renderer to the figure
 p.line(x=df['date'], y=df['value'], line_width=2)
 
+# bokeh.plotting.output_file()
+# bokeh.plotting.save(p, filename=f"{RTDIR}/help")
+
+html = bokeh.embed.file_html(p)
+# print(f"{html=}\n\n\n")
+
+# json = bokeh.embed.json_item(p)
+# print(f"{json=}")
+
 # show the figure
-show(p)
+bokeh.plotting.show(p)
