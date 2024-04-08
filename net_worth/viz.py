e72fa777344fad0593b8c8bca46dcb12b9d14528
SeanDuffie <sean.duffie@tamu.edu>
2024-04-07 21:00:32 -0400

    Got a graph with a decent selector

--- net_worth/viz.py
+++ net_worth/viz.py
@@ -1,34 +1,69 @@
 """_summary_
 """
 import os
+import numpy as np
 
 import bokeh.embed
-import bokeh.plotting
 import pandas as pd
-import pandas_bokeh
-from bokeh.models import ColumnDataSource, DatetimeTickFormatter
-from bokeh.layouts import layout
+from bokeh.models import ColumnDataSource, DatetimeTickFormatter, RangeTool
+from bokeh.layouts import layout, column
+from bokeh.plotting import figure, show
+
+# import bokeh.sampledata
+# bokeh.sampledata.download(True)
+
+from bokeh.sampledata.stocks import AAPL
 
 RTDIR = os.path.dirname(__file__)
 
-# create a pandas dataframe
-df = pd.DataFrame({'date': pd.to_datetime(['2023-01-01', '2023-01-02', '2023-01-03']), 'value': [1, 2, 3]})
+def norm_line():
+    # create a pandas dataframe
+    df = pd.DataFrame({'date': pd.to_datetime(['2023-01-01', '2023-01-02', '2023-01-03']), 'value': [1, 2, 3]})
 
-# create a Bokeh figure
-# p = bokeh.plotting.figure(title="Line Plot", x_axis_label='Date', y_axis_label='Value')
-p = bokeh.plotting.figure(title="Value")
+    # create a Bokeh figure
+    # p = bokeh.plotting.figure(title="Line Plot", x_axis_label='Date', y_axis_label='Value')
+    p = bokeh.plotting.figure(title="Value")
 
-# add a line renderer to the figure
-p.line(x=df['date'], y=df['value'], line_width=2)
+    # add a line renderer to the figure
+    p.line(x=df['date'], y=df['value'], line_width=2)
 
-# bokeh.plotting.output_file()
-# bokeh.plotting.save(p, filename=f"{RTDIR}/help")
+    # bokeh.plotting.output_file()
+    # bokeh.plotting.save(p, filename=f"{RTDIR}/help")
 
-html = bokeh.embed.file_html(p)
-# print(f"{html=}\n\n\n")
+    html = bokeh.embed.file_html(p)
+    # print(f"{html=}\n\n\n")
 
-# json = bokeh.embed.json_item(p)
-# print(f"{json=}")
+    # json = bokeh.embed.json_item(p)
+    # print(f"{json=}")
 
-# show the figure
-bokeh.plotting.show(p)
+    # show the figure
+    bokeh.plotting.show(p)
+
+def scroll_line():
+    dates = np.array(AAPL['date'], dtype=np.datetime64)
+    source = ColumnDataSource(data=dict(date=dates, close=AAPL['adj_close']))
+
+    p = figure(height=300, width=800, tools="xpan", toolbar_location=None,
+            x_axis_type="datetime", x_axis_location="below",
+            background_fill_color="#efefef", x_range=(dates[1500], dates[2500]))
+
+    p.line('date', 'close', source=source)
+    p.yaxis.axis_label = 'Price'
+
+    select = figure(title="Drag the middle and edges of the selection box to change the range above",
+                    height=130, width=800, y_range=p.y_range,
+                    x_axis_type="datetime", y_axis_type=None,
+                    tools="", toolbar_location=None, background_fill_color="#efefef")
+
+    range_tool = RangeTool(x_range=p.x_range)
+    range_tool.overlay.fill_color = "navy"
+    range_tool.overlay.fill_alpha = 0.2
+
+    select.line('date', 'close', source=source)
+    select.ygrid.grid_line_color = None
+    select.add_tools(range_tool)
+
+    show(column(p, select))
+
+if __name__ == "__main__":
+    scroll_line()
